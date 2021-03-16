8e3995a819503181ea71960bc362dc0739fcad59
Mollie Dickenson <molliedickenson@gmail.com>
2021-03-15 22:46:20 -0400

    commit tree 933a576f6ffab7477b12affc05243340f78591ae

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(num){
+  let result;
+  if (num <= 400) {
+    result = 'This one is on me!';
 
-function ternaryCheckCity(){
-  // Write your code here!
+  } else if (num > 2000 && 2500 > num) {
+    result = 'I will gladly take your thirty bucks.'
+  } else if (num > 2500) {
+    result = 'No can do.';
+  }
+  return result;
 }
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let destination;
+  city === "NYC" ? (destination = "Ok, sounds good.") : (destination = "No go.");
+  return destination;
+  
 }
+ternaryCheckCity('NYC');
+ternaryCheckCity('Pittsburgh');
+
+
+function switchOnCharmFromTip(tip){
+  let charm;
+  switch (tip) {
+    case 'generous' :
+      charm = 'Thank you so much.';
+      break;
+    case 'not as generous' :
+      charm = 'Thank you.';
+      break;
+    default:
+      charm = "Bye."
+  }
+  return charm
+}
+switchOnCharmFromTip('generous');
+switchOnCharmFromTip('not as generous');
+switchOnCharmFromTip('thanks for everything');
